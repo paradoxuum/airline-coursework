ae4b53da2978f81ca5adc9ed9a7c6d813138883e
paradoxuum <hello@paradoxum.dev>
2024-05-20 18:32:47 +0100

    build: use node SSR adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-	output: "hybrid",
-	experimental: {
-		actions: true,
-	},
-	integrations: [tailwind(), react()],
+  output: "hybrid",
+  experimental: {
+    actions: true
+  },
+  integrations: [tailwind(), react()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
